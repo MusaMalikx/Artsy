66cac869b3631109bceeab1f4b1ff9894aa92b92
Musa Malik <muhammadmusamalik2@gmail.com>
2022-11-07 00:02:53 +0500

    configuring unsplash

--- src/utils/unsplash.js
+++ src/utils/unsplash.js
@@ -3,7 +3,7 @@ import {createApi} from "unsplash-js"
 const API = createApi({
     // Don't forget to set your access token here!
     // See https://unsplash.com/developers
-    accessKey: process.env.REACT_APP_ACCESSKEY//"AVJFYUIdZksA6ohMJ4DVjGb6i-olvvisJzs4EoDA-ro"
+    accessKey: process.env.REACT_APP_ACCESSKEY
   });
 
 
