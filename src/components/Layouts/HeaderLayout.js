e969ac7442951413472b7c1989cb446da1094b16
MusaMalikx <muhammadmusamalik2@gmail.com>
2022-12-23 19:07:26 +0500

    Auction table renewed and bugs fixed

--- src/components/Layouts/HeaderLayout.js
+++ src/components/Layouts/HeaderLayout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeaderLayout = ({ title }) => {
   return (
-    <div className="py-10 px-5">
+    <div className="pt-10 px-5">
       <h1 className="uppercase tracking-widest text-lg md:text-2xl whitespace-nowrap">{title}</h1>
       <hr />
     </div>
