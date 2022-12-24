a4039588901c65f4873073ba903c3c003baf5c5a
MusaMalikx <muhammadmusamalik2@gmail.com>
2022-12-25 00:18:27 +0500

    The carousel added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        banner1: "url('/src/assets/images/banner/banner1.jpg')",
+        banner2: "url('/src/assets/images/banner/banner2.jpg')",
+        banner3: "url('/src/assets/images/banner/banner3.jpg')",
+        banner4: "url('/src/assets/images/banner/banner4.jpg')",
+        banner5: "url('/src/assets/images/banner/banner5.jpg')",
+        banner6: "url('/src/assets/images/banner/banner6.jpg')",
+        banner7: "url('/src/assets/images/banner/banner7.jpg')"
+      },
       colors: {
         primary: '#188796',
         dark: '#071e34',
