4704ec0ac32359fcdb0b28ba286c4e4f1630f70a
Muhammad Ahmed <62472263+ahmedazhar2299@users.noreply.github.com>
2023-01-01 01:09:35 +0500

    categories bar addded

--- src/pages/Auctions/Auctions.js
+++ src/pages/Auctions/Auctions.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import AuctionCard from '../../components/Auction/AuctionCard';
 import Layout from '../../components/Layouts/ArticleLayout';
-
+import HeaderLayout from '../../components/Layouts/HeaderLayout';
+import { Nav } from 'rsuite';
+const Navbar = ({ active, onSelect, ...props }) => {
+  return (
+    <Nav
+      className="w-full flex items-center pt-6"
+      {...props}
+      activeKey={active}
+      onSelect={onSelect}
+      style={{ marginBottom: 50 }}>
+      <p className="text-xl pb-3 ml-6 font-bold text-primary">Categories</p>
+      <div className="text-base w-full flex justify-evenly">
+        <Nav.Item eventKey="home">Modern</Nav.Item>
+        <Nav.Item eventKey="Religious">Religious</Nav.Item>
+        <Nav.Item eventKey="Calligraphy">Calligraphy</Nav.Item>
+        <Nav.Item eventKey="Cubism">Cubism</Nav.Item>
+        <Nav.Item eventKey="Fantasy">Fantasy</Nav.Item>
+        <Nav.Item eventKey="Grapfitti">Grapfitti</Nav.Item>
+        <Nav.Item eventKey="Sculpture">Sculpture</Nav.Item>
+      </div>
+    </Nav>
+  );
+};
 const Auctions = ({ data }) => {
+  const [active, setActive] = React.useState('home');
   return (
     <Layout title={'Auctions'}>
-      <div className="py-10 px-5">
-        <p className="font-semibold   text-2xl lg:text-4xl">Auctions</p>
-        <hr />
-      </div>
+      <HeaderLayout title={'Auctions'} />
+      <Navbar appearance="tabs" active={active} onSelect={setActive} />
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 px-5">
         {data?.map((photo) => (
           <AuctionCard key={photo.id} photo={photo} />
