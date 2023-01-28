b07ca3422e35518fbe8e1cd0ca065040327c3583
MusaMalikx <muhammadmusamalik2@gmail.com>
2023-01-28 15:19:10 +0500

    realtime chat completed

--- src/pages/Dashboard/BuyerProfileDashboard.js
+++ src/pages/Dashboard/BuyerProfileDashboard.js
@@ -16,6 +16,7 @@ import { getAuth, signOut } from 'firebase/auth';
 import Toaster from '../../components/Common/Toaster';
 import { useToaster } from 'rsuite';
 import API from '../../api/server';
+import ReactJdenticon from 'react-jdenticon';
 
 export default function BuyerProfileDashboard({ data }) {
   const toaster = useToaster();
@@ -98,12 +99,15 @@ export default function BuyerProfileDashboard({ data }) {
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
                     <div className="relative w-full text-center flex justify-center">
-                      <img
+                      <div className="shadow-xl object-cover align-middle border-none absolute -m-20 -ml-24 md:-mt-24 max-w-200 bg-white">
+                        <ReactJdenticon size="200" value={auth?.user.email} />
+                      </div>
+                      {/* <img
                         src={profileInfo.profileImage}
                         className="shadow-xl rounded-full h-36 w-36 md:h-auto md:w-48 object-cover align-middle border-none absolute -m-20 -ml-24 md:-mt-24 max-w-200-px"
                         alt="profile"
                         srcSet=""
-                      />
+                      /> */}
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,19 +2,38 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import { enableIndexedDbPersistence, getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_API,
   appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
+
+enableIndexedDbPersistence(db).catch((err) => {
+  if (err.code == 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled
+    // in one tab at a a time.
+    // ...
+  } else if (err.code == 'unimplemented') {
+    // The current browser does not support all of the
+    // features required to enable persistence
+    // ...
+  }
+});
 
 export default app;
