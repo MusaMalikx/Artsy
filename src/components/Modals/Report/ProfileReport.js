df545be810922a791ce90a66ed11a1df9d236be0
Abdullah Tahir <abd.tahir1122@gmail.com>
2023-03-27 01:34:13 +0500

    Report functionality added for report buyer or report artist

--- src/components/Modals/Report/ProfileReport.js
+++ src/components/Modals/Report/ProfileReport.js
@@ -1,27 +1,85 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Checkbox, Modal } from 'rsuite';
 import { AiFillFlag } from 'react-icons/ai';
 import { useToaster } from 'rsuite';
 import Toaster from '../../Common/Toaster';
-import { descriptionValidate } from '../../../helpers/proposal-validators';
+import { descriptionValidate, categoryValidate } from '../../../helpers/proposal-validators';
+import API from '../../../api/server';
+import { useLocation } from 'react-router-dom';
+// import { auth } from '../../../utils/firebase';
 
 export default function ProfileReport({ open, setOpen }) {
-  const handleClose = () => setOpen(false);
+  const handleClose = () => {
+    setCategoriesCheckbox([]);
+    setOpen(false);
+  };
+  const location = useLocation();
+  const currentUserID = location.pathname.split('/')[3];
   const description = useRef();
   const toaster = useToaster();
+  const [categoriesCheckbox, setCategoriesCheckbox] = useState([]);
+  const auth = JSON.parse(localStorage.getItem('auth'));
 
-  const submitReport = (e) => {
-    if (descriptionValidate(description.current.value)) {
+  const submitReport = async (e) => {
+    let url = '';
+    let body = {};
+    if (auth.usertype === 'buyer') {
+      url = '/api/users/report/artist';
+      body = {
+        category: categoriesCheckbox,
+        description: description.current.value,
+        artistid: currentUserID
+      };
+    } else {
+      url = '/api/artists/report/buyer';
+      body = {
+        category: categoriesCheckbox,
+        description: description.current.value,
+        buyerid: currentUserID
+      };
+    }
+
+    if (
+      descriptionValidate(description.current.value) &&
+      categoryValidate(categoriesCheckbox.length)
+    ) {
       e.preventDefault();
       //Write Axios Code here
-
-      Toaster(toaster, 'success', 'Successfully Reported');
+      await API.post(url, body, {
+        headers: {
+          token: 'Bearer ' + auth.token
+        }
+      })
+        .then((res) => {
+          console.log(res.data);
+          handleClose();
+          Toaster(toaster, 'success', 'Successfully Reported');
+        })
+        .catch((err) => {
+          Toaster(toaster, 'error', err.response.data.message);
+        });
+    } else if (!categoryValidate(categoriesCheckbox.length)) {
+      e.preventDefault();
+      description.current.setCustomValidity('');
+      Toaster(toaster, 'error', 'Select a option!');
     } else {
       !descriptionValidate(description.current.value)
         ? description.current.setCustomValidity(
             'Description must have a minimum length of 10 characters'
           )
         : description.current.setCustomValidity('');
+    }
+  };
+
+  const handleChange = (value, checked) => {
+    // console.log(`${value} is ${checked}`);
+    // Case 1 : The user checks the box
+    if (checked) {
+      setCategoriesCheckbox((prev) => [...prev, value]);
+    }
+    // Case 2  : The user unchecks the box
+    else {
+      setCategoriesCheckbox((prev) => prev.filter((item) => item !== value));
     }
   };
   return (
@@ -40,14 +98,50 @@ export default function ProfileReport({ open, setOpen }) {
         <form action="#">
           <div className="flex flex-col w-full">
             <div className="flex ">
-              <Checkbox className="w-1/2"> Fake Profile</Checkbox>
-              <Checkbox className="w-1/2"> Scam</Checkbox>
-              <Checkbox className="w-1/2"> Inappropriate Behaviour</Checkbox>
+              <Checkbox
+                className="w-1/2"
+                value="Fake Profile"
+                onChange={(value, checked) => handleChange(value, checked)}>
+                {' '}
+                Fake Profile
+              </Checkbox>
+              <Checkbox
+                className="w-1/2"
+                value="Scam"
+                onChange={(value, checked) => handleChange(value, checked)}>
+                {' '}
+                Scam
+              </Checkbox>
+              <Checkbox
+                className="w-1/2"
+                value="Inappropriate Behaviour"
+                onChange={(value, checked) => handleChange(value, checked)}>
+                {' '}
+                Inappropriate Behaviour
+              </Checkbox>
             </div>
             <div className="flex ">
-              <Checkbox className="w-1/2"> Hateful Content</Checkbox>
-              <Checkbox className="w-1/2"> Misleading</Checkbox>
-              <Checkbox className="w-1/2"> Spam Messages</Checkbox>
+              <Checkbox
+                className="w-1/2"
+                value="Hateful Content"
+                onChange={(value, checked) => handleChange(value, checked)}>
+                {' '}
+                Hateful Content
+              </Checkbox>
+              <Checkbox
+                className="w-1/2"
+                value="Misleading"
+                onChange={(value, checked) => handleChange(value, checked)}>
+                {' '}
+                Misleading
+              </Checkbox>
+              <Checkbox
+                className="w-1/2"
+                value="Spam Messages"
+                onChange={(value, checked) => handleChange(value, checked)}>
+                {' '}
+                Spam Messages
+              </Checkbox>
             </div>
           </div>
           <div className="mt-2 flex flex-col gap-2">
