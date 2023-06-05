2ac2e5d0bdc75a9638730f662a7b7ad54cb2a838
MusaMalikx <muhammadmusamalik2@gmail.com>
2023-06-05 08:44:00 +0500

    loader added in admin dashboard

--- src/pages/Dashboard/AdminDashboard.js
+++ src/pages/Dashboard/AdminDashboard.js
@@ -9,6 +9,7 @@ import Graphs from '../../components/Dashboard/Admin/Graphs';
 import AdminLayout from '../../components/Layouts/AdminLayout';
 import HeaderLayout from '../../components/Layouts/HeaderLayout';
 import API from '../../api/server';
+import { Loader } from 'rsuite';
 
 /*
 This component renders the admin dashboard interface, providing access to various administrative features and functionalities.
@@ -16,16 +17,19 @@ The layout and design are optimized for ease of use and efficient administration
 */
 const AdminDashboard = () => {
   const [stat, setStat] = useState();
+  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
     const getCount = async () => {
+      setLoader(true);
       await API.get('/api/users/count')
         .then((res) => {
           setStat(res.data);
         })
         .catch((err) => {
           console.log(err);
-        });
+        })
+        .finally(() => setLoader(false));
     };
 
     getCount();
@@ -36,14 +40,20 @@ const AdminDashboard = () => {
   return (
     <AdminLayout title="Auctions" bool>
       <HeaderLayout title="Dashboard" />
-      <div className="py-10 flex flex-col gap-4 mx-10 relative">
-        <Stats stat={stat} />
-        <div className="flex flex-col mx-auto items-center justify-center mt-10">
-          <Profile />
-          <Info stats={stat} />
-          <Graphs stats={stat} />
+      {loader ? (
+        <div className="flex justify-center mt-10">
+          <Loader size="lg" />
         </div>
-      </div>
+      ) : (
+        <div className="py-10 flex flex-col gap-4 mx-10 relative">
+          <Stats stat={stat} />
+          <div className="flex flex-col mx-auto items-center justify-center mt-10">
+            <Profile />
+            <Info stats={stat} />
+            <Graphs stats={stat} />
+          </div>
+        </div>
+      )}
       {/* <div className="fixed bottom-16 flex justify-center w-full">
         <Navigation />
       </div> */}
